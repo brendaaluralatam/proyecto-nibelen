4e7ec6df70d024139ccefbe480365ddd25f8bd34
Maria Fernanda Ferreira <53662778+mariafernandafs@users.noreply.github.com>
2023-06-16 10:12:03 -0300

    Add files via upload
    criação do projeto base nibelen

---
+++ nibelen_textEditor_base/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.view.MainWindow;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+}
